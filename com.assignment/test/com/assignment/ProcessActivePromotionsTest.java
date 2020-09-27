ca9b9598952585b982122e2304f4fc27e1d03419
uib08758 <uib08758@OZL2988W.cw01.contiwan.com>
2020-09-27 16:30:49 +0530

    ProcessActivePromotionsTest

---
+++ com.assignment/test/com/assignment/ProcessActivePromotionsTest.java
@@ -0,0 +1,26 @@
+package com.assignment;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Nagaraj
+ *
+ */
+class ProcessActivePromotionsTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
