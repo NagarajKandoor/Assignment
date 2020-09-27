da77fee680a84329682e785286aabb1b3a0d68ec
uib08758 <uib08758@OZL2988W.cw01.contiwan.com>
2020-09-27 15:18:16 +0530

    CalculateOrderValueTest

---
+++ com.assignment/test/com/assignment/CalculateOrderValueTest.java
@@ -0,0 +1,26 @@
+package com.assignment;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Nagaraj
+ *
+ */
+class CalculateOrderValueTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
