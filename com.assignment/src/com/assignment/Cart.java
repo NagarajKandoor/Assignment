14ee6194aaf0107f1910e75dd8c2a175e431d1c2
uib08758 <uib08758@OZL2988W.cw01.contiwan.com>
2020-09-27 14:48:22 +0530

    Cart Model

---
+++ com.assignment/src/com/assignment/Cart.java
@@ -0,0 +1,26 @@
+package com.assignment;
+
+import java.util.List;
+
+/**
+ * @author Nagaraj
+ *
+ */
+public class Cart {
+	
+	private List<Character> cartItems;
+
+	/**
+	 * @return the cartItems
+	 */
+	public List<Character> getCartItems() {
+		return cartItems;
+	}
+
+	/**
+	 * @param cartItems the cartItems to set
+	 */
+	public void setCartItems(List<Character> cartItems) {
+		this.cartItems = cartItems;
+	}
+}
---
+++ com.assignment/test/com/assignment/CartTest.java
@@ -0,0 +1,45 @@
+package com.assignment;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Nagaraj
+ *
+ */
+class CartTest {
+	private Cart cart;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		cart = new Cart();
+		List<Character> cartItems = new ArrayList<Character>();
+		cartItems.add('A');
+		cartItems.add('B');
+		cartItems.add('C');
+		cart.setCartItems(cartItems);
+	}
+
+	@Test
+	final void testGetCartItemsPostive() {
+		assertEquals(3, cart.getCartItems().size());
+	}
+
+	@Test
+	final void testGetCartItemsNegative() {
+		assertNotEquals(2, cart.getCartItems().size());
+	}
+
+	@AfterEach
+	void clear() {
+		cart = null;
+	}
+}
